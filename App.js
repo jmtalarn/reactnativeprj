852d4ae653e7b18ca96eb5c9ac0c24d641b0d3bc
Joan Maria Talarn Espelta <jmtalarn@gmail.com>
2018-10-14 11:00:12 +0200

    Added dev tools

--- App.js
+++ App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,8 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Hello World!</Text>
+        <Text style={styles.instructions}>I have no idea what I'm doing. Just editing App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
@@ -31,19 +31,19 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    // flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: 'white',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    // fontSize: 20,
+    // textAlign: 'center',
+    // margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    // textAlign: 'center',
+    // color: '#333333',
+    // marginBottom: 5,
   },
 });
